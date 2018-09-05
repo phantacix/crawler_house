4e7fc4776b6e4f52c4e772473b6037d12e0535e6
root <root@localhost>
2018-09-05 17:10:44 +0800

    优化： 1、设置 Chrome 以 headless 模式运行 2、不加载图片 3、禁用 GPU

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"github.com/tebeka/selenium"
 	"log"
+	"github.com/tebeka/selenium/chrome"
 )
 
 
@@ -13,8 +14,27 @@ func Fetch(url string)(driver selenium.WebDriver) {
 
 	opts := []selenium.ServiceOption{
 		selenium.Output(os.Stderr), // Output debug information to STDERR.
+
 	}
 	caps := selenium.Capabilities{"browserName": "chrome"}
+
+
+	// 禁止加载图片，加快渲染速度
+	imagCaps := map[string]interface{}{
+		"profile.managed_default_content_settings.images": 2,
+	}
+
+	chromeCaps := chrome.Capabilities{
+		Prefs: imagCaps,
+		Path:  "",
+		Args: []string{
+			"--headless", // 设置Chrome无头模式
+			"--disable-gpu",
+			//"--no-sandbox",
+			"--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7", // 模拟user-agent，防反爬
+		},
+	}
+	caps.AddChrome(chromeCaps)
 
 	selenium.SetDebug(false)
 	_, err := selenium.NewChromeDriverService(CHROME_DRIVER_PATH, 9515, opts...)
