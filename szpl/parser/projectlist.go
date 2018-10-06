e579dfab85160724f544c6255fb5f68c88d82b2c
devops <devops@mygithub.com>
2018-10-06 10:44:21 +0800

    修改 seeds 生成方式

--- szpl/parser/projectlist.go
+++ szpl/parser/projectlist.go
@@ -2,6 +2,8 @@ package parser
 
 import (
 	"fmt"
+	"strings"
+	"strconv"
 	"github.com/tebeka/selenium"
 	"../../engine"
 )
@@ -15,7 +17,8 @@ func ParseProjectList(driver selenium.WebDriver) engine.ParseResult {
 	if err != nil {
 		panic(err)
 	}
-	pageNum, _ := pages.Text()
+	pn, _ := pages.Text()
+	pageNum, _ := strconv.Atoi(pn)
 	fmt.Println("Total pages: ", pageNum) //Total pages:  106
 
 	// 获取总项目数
@@ -34,19 +37,49 @@ func ParseProjectList(driver selenium.WebDriver) engine.ParseResult {
 	project, _ := projects.Text()
 	fmt.Println("Total projects: ", project)
 
-	a, err := webDriver.FindElements(selenium.ByXPATH, "//a")
-	if err != nil{
-		panic(err)
-	}
-	result := engine.ParseResult{}
+	//a, err := webDriver.FindElements(selenium.ByXPATH, "//a")
+	//if err != nil{
+	//	panic(err)
+	//}
+	//result := engine.ParseResult{}
+	//
+	////  下面这段用于减少 seeds， 只爬一个项目
+	//fmt.Printf("%v", a)
+	//result.Items = append(result.Items, "华润城润府三期")
+	//result.Requests = append(result.Requests, engine.Request{
+	//	Url: string("http://ris.szpl.gov.cn/bol/projectdetail.aspx?id=34432"), // 调试
+	//	ParserFunc: ParseProject,
+	//})
 
-	//  下面这段用于减少 seeds， 只爬一个项目
-	fmt.Printf("%v", a)
-	result.Items = append(result.Items, "华润城润府三期")
-	result.Requests = append(result.Requests, engine.Request{
-		Url: string("http://ris.szpl.gov.cn/bol/projectdetail.aspx?id=34432"), // 调试
-		ParserFunc: ParseProject,
-	})
+	result := engine.ParseResult{}
+	for i := 0; i < pageNum - 106; i++{
+		wd, err := webDriver.FindElement(selenium.ByXPATH, "//*[@id=\"AspNetPager1\"]/div[2]/a[3]")
+		if err != nil{
+			panic(err)
+		}
+		wd.Click()
+		a, err := webDriver.FindElements(selenium.ByXPATH, "//a")
+		//if err != nil{
+		//	panic(err)
+		//}
+		for _, v := range a {
+			href, _ := v.GetAttribute("href")
+			p, _ := v.Text()
+			if p != "" {
+				// 匹配 url 中的 projectdetail 过滤掉许可证详细信息页面
+				// http://ris.szpl.gov.cn/bol/projectdetail.aspx?id=34792
+				// http://ris.szpl.gov.cn/bol/certdetail.aspx?id=35373
+				if strings.Contains(href, "projectdetail") {
+					fmt.Printf("超链接地址：%s, 项目：%s \n", href, p)
+					result.Items = append(result.Items, p)
+					result.Requests = append(result.Requests, engine.Request{
+						Url: string(href),
+						ParserFunc: ParseProject,
+					})
+				}
+			}
+		}
+	}
 
 	//for _, v := range a {
 	//	href, _ := v.GetAttribute("href")
